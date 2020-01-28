528307f71d488e17e4e01fdf94875c84f10eb994
tam5 <arimiller92@gmail.com>
2020-01-28 13:35:05 -0500

    more estimates

--- src/components/ProgressBar.vue
+++ src/components/ProgressBar.vue
@@ -1,17 +1,94 @@
 <template>
     <transition name="fade">
-        <div class="h-8 w-1/2 mx-auto bg-gray-300 rounded">
-            <div class="progress bg-pink-300 h-full rounded" :style="`width: ${current/total * 100}%;`"></div>
-            <div class="text-sm flex justify-center">{{ current }}/{{ total }} chunks - {{ current / total * 100}}%</div>
+        <div>
+            <div class="h-8 w-1/2 mx-auto bg-gray-300 rounded">
+                <div class="progress bg-pink-300 h-full rounded" :style="`width: ${percent}%;`"></div>
+            </div>
+
+            <div class="text-sm flex justify-center">{{ current }}/{{ total }} chunks - {{ percent }}%</div>
+            <div class="text-sm flex justify-center">
+                Time Elapsed: ~{{ elapsedSeconds }} seconds
+            </div>
+            <div class="text-sm flex justify-center">
+                Time Remaining: ~{{ remaining }}
+            </div>
+            <div class="text-sm flex justify-center">
+                Average Chunk Time: {{ averageChunkTime.toFixed(2) }} seconds
+            </div>
         </div>
     </transition>
 </template>
 
 <script>
+ import { toHHMMSS } from '@/utils';
+
  export default {
      name: 'ProgressBar',
 
-     props: ['current', 'total']
+     props: ['current', 'total'],
+
+     data() {
+         return {
+             previousTimestamp: Date.now(),
+             durations: [0],
+             elapsedSeconds: 0,
+             interval: null
+         }
+     },
+
+     computed: {
+         isDone() {
+             return this.current === this.total
+         },
+
+         percent() {
+             return parseFloat(
+                 ((this.current / this.total) * 100).toFixed(2)
+             )
+         },
+
+         averageChunkTime() {
+             const sum = this.durations.reduce((a, b) => a + b, 0)
+             return (sum / this.durations.length) || 0;
+         },
+
+         remaining() {
+             return toHHMMSS(this.averageChunkTime * (this.total - this.current))
+         }
+     },
+
+     watch: {
+         current() {
+             const now = Date.now()
+             const duration = (now - this.previousTimestamp) / 1000
+
+             this.$set(this.durations, this.durations.length, duration)
+
+             this.previousTimestamp = now
+         },
+
+         total() {
+             this.initialize()
+         }
+     },
+
+     methods: {
+         initialize() {
+             this.durations = []
+
+             this.interval = setInterval(() => {
+                 this.elapsedSeconds++;
+
+                 if (this.isDone) {
+                     clearInterval(this.interval)
+                 }
+             }, 1000)
+         }
+     },
+
+     created() {
+         this.initialize()
+     }
  }
 </script>
 
--- src/utils.js
+++ src/utils.js
@@ -4,6 +4,25 @@
 export function mb(bytes) {
     return bytes / 1024 / 1024
 }
+/**
+ * Convert byes to megabytes.
+ */
+export function toHHMMSS(seconds) {
+    // multiply by 1000 because Date() requires miliseconds
+    var date = new Date(seconds * 1000);
+    var hh = date.getUTCHours();
+    var mm = date.getUTCMinutes();
+    var ss = date.getSeconds();
+    // If you were building a timestamp instead of a duration, you would uncomment the following line to get 12-hour (not 24) time
+    // if (hh > 12) {hh = hh % 12;}
+    // These lines ensure you have two-digits
+    if (hh < 10) {hh = "0"+hh;}
+    if (mm < 10) {mm = "0"+mm;}
+    if (ss < 10) {ss = "0"+ss;}
+
+    // This formats your string to HH:MM:SS
+    return hh+":"+mm+":"+ss;
+}
 
 /**
  * Convert byes to megabytes.
